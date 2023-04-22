17882d170ee5f4c8be3cbe4b96ba08cf4c02a051
PhysBoom <physboom@gmail.com>
2023-04-22 09:31:37 -0700

    Basic setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./index.css"
+
+function App() {
+  return (
+    <h1 className="text-red-500">Hello World!</h1>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#121212",
+        light: "#f0f0f0"
+      }
+    },
+  },
+  plugins: [],
+}
+
